42db544c9b2d6c37ffa27cd45f736e2a2a76696e
lxyxyf <L13235339564@163.com>
2022-07-27 16:42:01 +0800

    firstsubmit

---
+++ yuejianghu/yuejianghu/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  yuejianghu
+//
+//  Created by 李欣烨 on 2022/7/27.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
